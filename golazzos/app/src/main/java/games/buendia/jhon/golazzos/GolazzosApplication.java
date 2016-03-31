cff009913e1219ad6ba487c1862d2076e51e7ac6
Jose Luis Rodriguez Contreras <jlpuma24@gmail.com>
2016-03-31 13:56:45 -0500

    Adding analytics configuration

--- golazzos/app/src/main/java/games/buendia/jhon/golazzos/GolazzosApplication.java
+++ golazzos/app/src/main/java/games/buendia/jhon/golazzos/GolazzosApplication.java
@@ -1,6 +1,9 @@
 package games.buendia.jhon.golazzos;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.flurry.android.FlurryAgent;
 
 /**
  * Created by User on 03/02/2016.
@@ -13,6 +16,13 @@ public class GolazzosApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
+        new FlurryAgent.Builder()
+                .withLogEnabled(true)
+                .withLogLevel(Log.INFO)
+                .withContinueSessionMillis(5000L)
+                .withCaptureUncaughtExceptions(false)
+                .withPulseEnabled(true)
+                .build(context, "G3N7XTG6P6W5PVF8BKC5");
     }
 
     public static GolazzosApplication getInstance() {
